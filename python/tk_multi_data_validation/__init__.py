535e9635d3137fd6c642a1869600825787a4d6b0
Stacey Oue <stacey.oue@autodesk.com>
2022-06-17 12:34:15 -0400

    Temporary work around to run unit tests while exposing widget class.

--- python/tk_multi_data_validation/__init__.py
+++ python/tk_multi_data_validation/__init__.py
@@ -9,7 +9,14 @@
 # not expressly granted therein are reserved by Autodesk, Inc.
 
 from .api import ValidationManager
-from .widgets import ValidationWidget
+
+try:
+    from .widgets import ValidationWidget
+except:
+    # Temporary work around to allow pytests to run. Unit tests do not have
+    # access to sgtk.platform.qt for QtCore and QtGui so attempting to import
+    # any classes that requires qt will fail while running tests.
+    pass
 
 
 def show_dialog(app, modal=False):
