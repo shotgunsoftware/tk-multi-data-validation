9d6391db450d41c652dc2bd0ef90eccf29eab4ee
Stacey Oue <stacey.oue@autodesk.com>
2022-09-19 16:14:02 -0400

    Remove unnecessary property for pre validate.

--- python/tk_multi_data_validation/api/manager.py
+++ python/tk_multi_data_validation/api/manager.py
@@ -38,7 +38,6 @@ class ValidationManager(object):
         logger=None,
         notifier=None,
         has_ui=False,
-        pre_validate_before_fix=True,
     ):
         """
         Initialize the validation manager from the settings data.
@@ -63,9 +62,6 @@ class ValidationManager(object):
         :type notifier: ValidationNotifer
         :param has_ui: Set to True if the manager is being used with a UI, else False.
         :type has_ui: bool
-        :param pre_validate_before_fix: Set to True to ensure validate is executed before a
-            rule is fixed.
-        :type pre_validate_before_fix: bool
 
         :signal ValidationNotifier.validate_rule_begin(ValidationRule): Emits before a validation rule check
             function is executed. The returned parameter is the validation rule.
@@ -77,7 +73,6 @@ class ValidationManager(object):
         self._logger = logger or self._bundle.logger
         self._notifier = notifier
         self._has_ui = has_ui
-        self._pre_validate_before_fix = pre_validate_before_fix
 
         # Set the default rule types (in order). This can be set using the rule_types property.
         # TODO allow this to be config-based
@@ -175,22 +170,6 @@ class ValidationManager(object):
     def has_ui(self):
         """Get the flag indicating if this manager is running with a User Interface."""
         return self._has_ui
-
-    @property
-    def pre_validate_before_fix(self):
-        """
-        Get or set the property to run validate before fix.
-
-        Set to True to ensure that validate is always executed before a rule is fixed. This
-        ensure that the fix is applied to the most up to date data.
-
-        Default is True.
-        """
-        return self._pre_validate_before_fix
-
-    @pre_validate_before_fix.setter
-    def pre_validate_before_fix(self, pre_validate):
-        self._pre_validate_before_fix = pre_validate
 
     #########################################################################################################
     # Public functions
