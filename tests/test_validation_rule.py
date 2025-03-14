fcab0c53256694d5f8032a7c296353c893350ff1
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2025-03-14 15:02:31 -0500

    Replace mock

--- tests/test_validation_rule.py
+++ tests/test_validation_rule.py
@@ -12,7 +12,7 @@ import os
 import sys
 
 import pytest
-from mock import MagicMock
+from unittest.mock import MagicMock
 
 # Manually add the app modules to the path in order to import them here.
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "python"))
